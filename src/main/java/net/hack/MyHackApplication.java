f1061332d2c10729e5483d2f915bc29e6e0d231d
XTRM_LG\USER <saturn5903@gmailcom>
2018-05-24 14:38:59 +0900

    dfdsfds

---
+++ src/main/java/net/hack/MyHackApplication.java
@@ -0,0 +1,14 @@
+package net.hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHackApplication.class, args);
+	}
+}
+
+
